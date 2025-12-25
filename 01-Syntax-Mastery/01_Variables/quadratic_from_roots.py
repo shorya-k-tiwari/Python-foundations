db555561d3477bf624c8a9afe97f5de063d5295e
Shorya <shoryatiwari0409@gmail.com>
2025-12-26 00:27:37 +0530

    feat: implement quadratic equation generator with pretty-printing logic

---
+++ 01-Syntax-Mastery/01_Variables/quadratic_from_roots.py
@@ -0,0 +1,36 @@
+''' Program to generate a quadratic equation from the given roots '''
+
+# Taking roots of the quadratic equation as input from the user
+r1 = float(input("Enter the first root: "))
+r2 = float(input("Enter the second root: "))
+
+# Assigning coefficients 
+b = -(r1 + r2) 
+c = r1 * r2 
+
+# Formatting coefficients for display
+b_str= int(b) if b.is_integer() else b
+c_str= int(c) if c.is_integer() else c
+
+# Formatting coefficient of x
+if  b_str == 1:
+    d = f"+ x"
+elif b_str == 0:
+    d = ""
+elif b_str == -1:
+    d = "- x"
+elif b_str > 0:
+    d = f"+ {b_str}x"
+else:
+    d = f"- {abs(b_str)}x"
+
+# Formatting constant term
+if c_str > 0:
+    e = f"+ {c_str}"
+elif c_str == 0:
+    e = ""
+else:
+    e = f"- {abs(c_str)}"
+
+# Displaying the quadratic equation
+print(f"The quadratic equation is:\n x^2 {d} {e} = 0")
