850cc26449d7c124058d9080cba05657a5fa1ef6
Shorya <shoryatiwari0409@gmail.com>
2026-01-08 22:41:57 +0530

    fix: program

--- 01-Syntax-Mastery/01_Variables/type_inspector.py
+++ 01-Syntax-Mastery/01_Variables/type_inspector.py
@@ -1,21 +1,17 @@
-'''
-Type Inspector 
+"""
+Type Inspector
+Collects user input, converts values to basic data types, and displays their types
+"""
 
-Type Inspector is a simple script that collects user inputs, 
-converts them to appropriate types, and prints each variable's value and data type
-'''
-
-# Collect user input values
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 height = float(input("Enter your height in meters: "))
-student = input("Are you a student? (yes/no): ")
+student_response = input("Are you a student? (yes/no): ").strip().lower()
 
-# Convert student response to boolean value
-is_student = True if student.lower() == 'yes' else False
+is_student = student_response == "yes"
 
-# Print variable values and types
-print(f'Name -> {name} | Type: {type(name)}')
-print(f'Age -> {age} | Type: {type(age)}')
-print(f'Height -> {height} | Type: {type(height)}')
-print(f'Student -> {is_student} | Type: {type(is_student)}')
+print('\n', '|======== Output ========|')
+print(f"Name    : {name} | Type -> {type(name)}")
+print(f"Age     : {age} | Type -> {type(age)}")
+print(f"Height  : {height} | Type -> {type(height)}")
+print(f"Student : {is_student} | Type -> {type(is_student)}")
