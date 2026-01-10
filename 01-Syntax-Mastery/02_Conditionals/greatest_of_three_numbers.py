b545a03aa1c5ff7c1d83abecd3593c6cb811834a
Shorya <shoryatiwari0409@gmail.com>
2026-01-10 23:18:14 +0530

    program fixed

--- 01-Syntax-Mastery/02_Conditionals/greatest_of_three_numbers.py
+++ 01-Syntax-Mastery/02_Conditionals/greatest_of_three_numbers.py
@@ -1,26 +1,21 @@
-'''
+"""
 Greatest of Three Numbers
 
-Greatest of Three Numbers is the maximum value among three given numbers, 
-identified through direct comparisons like if A ≥ B and A ≥ C
-'''
+Determines the maximum value among three integers
+using direct conditional comparisons
+"""
 
-# Get inputs
 a = int(input('Enter First Number: '))
 b = int(input('Enter Second Number: '))
 c = int(input('Enter Third Number: '))
 
-# Check if all are equal
 if a == b == c:
-    print('All numbers are equal')
-
-# Determine the greatest number
+    greatest = a
 elif a >= b and a >= c:
-    n = a
+    greatest = a
 elif b >= a and b >= c:
-    n = b
+    greatest = b
 else:
-    n = c
+    greatest = c
 
-# Printing Result
-print(f'Greatest Number: {n}')
+print(f'Greatest Number: {greatest}')
