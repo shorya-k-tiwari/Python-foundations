fd78d12bfe3bdda1adcbad47d27ec5e2270515ae
Shorya <shoryatiwari0409@gmail.com>
2026-01-24 10:38:44 +0530

    feat: added power calculator

---
+++ 01-Syntax-Mastery/01_Variables/power_calculator.py
@@ -0,0 +1,12 @@
+'''
+Power Calculator
+
+Computes the value of a number raised to a given power
+'''
+
+x = float(input("Enter the base number: "))
+n = float(input("Enter the exponent: "))
+
+result = x ** n
+
+print(f"Result: {result:.2f}")
