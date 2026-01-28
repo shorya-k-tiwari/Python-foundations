a83921e889c74c8ade8330216b2cfabd841837d1
Shorya <shoryatiwari0409@gmail.com>
2026-01-28 18:14:12 +0530

    feat: added reverse number generator

--- 01-Syntax-Mastery/03_Loops/reverse_number_generator.py
+++ 01-Syntax-Mastery/03_Loops/reverse_number_generator.py
@@ -1,4 +1,23 @@
-'''
+"""
 Reverse Number Generator
-'''
 
+Reverses an integer while preserving its sign using iteration.
+"""
+
+num = int(input("Enter an integer: "))
+negative = False
+
+if num < 0:
+    negative = True
+    num = -num
+
+reverse = 0
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num //= 10
+
+if negative:
+    reverse = -reverse
+
+print(f"Reversed: {reverse}")
