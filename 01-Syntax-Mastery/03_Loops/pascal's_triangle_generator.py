38e3a24706b37935b97fd37a3095cca668c9d5ad
Shorya <shoryatiwari0409@gmail.com>
2026-01-30 19:48:51 +0530

    feat: added pascal's triangle generator

--- 01-Syntax-Mastery/03_Loops/pascal's_triangle_generator.py
+++ 01-Syntax-Mastery/03_Loops/pascal's_triangle_generator.py
@@ -1,4 +1,17 @@
 '''
 Pascal's Triangle Generator
+
+Generates Pascal's Triangle up to a given number of rows using iterative calculation
 '''
 
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows):
+
+    print(" " * (rows - i), end="")
+
+    value = 1
+    for j in range(i + 1):
+        print(value, end=" ")
+        value = value * (i - j) // (j + 1)
+    print()
